cecb74baaacbe90434c99a06b94978cde71c26e8
Fatemaakterlima9576 <fatema09876akter@gmail.com>
2024-12-14 10:03:28 +0600

    Add files via upload

---
+++ portfolio webside/index.js
@@ -0,0 +1,13 @@
+function webdesign(){
+    alert(" Do You want");
+}
+
+
+function webdevelop(){
+  let result =  confirm("Do You want to Confrim");
+document.write(result);    
+}
+
+function Teaching(){
+    confirm("Do you want to Lern")
+}
